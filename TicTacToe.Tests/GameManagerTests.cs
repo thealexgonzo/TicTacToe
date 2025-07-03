806820cf926e1e6e617688f7f3ac6ac727b61308
Alejandro González <65364812+thealexgonzo@users.noreply.github.com>
2025-07-03 16:08:54 +0100

    I've set up the basic structure of the solution, including the classes I'll use, the ClassLibrary project for testing, the interfaces, and the Factory class to instantiate players.

---
+++ TicTacToe.Tests/GameManagerTests.cs
@@ -0,0 +1,10 @@
+﻿using NUnit.Framework;
+
+namespace TicTacToe.Tests
+{
+    [TestFixture]
+    public class GameManagerTests
+    {
+
+    }
+}
---
+++ TicTacToe.UI/Result.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.UI
+{
+    public enum Result
+    {
+        SymbolPlace,
+        XWins,
+        OWins,
+        Draw,
+        InvalidOverlap,
+        InvalidOffGrid
+    }
+}
